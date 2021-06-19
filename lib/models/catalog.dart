63ef5d8b881f2760e436d5097742c4295d1dc586
sawantansh17 <sawantansh17@gmail.com>
2021-06-19 17:07:15 +0530

    Day 12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,19 @@
+class CatalogModel {
+  static final items = [
+    Item(
+      id:1, 
+      name:"Iphone 12 pro", 
+      desc:"an apple phone", 
+      price:46, 
+      color:"#33505a", 
+      image:"https://images.unsplash.com/photo-1537589376225-5405c60a5bd8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+    )
+  ];
+}
+
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -9,13 +23,3 @@ class Item {
   Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
 }
 
-final products = [
-  Item(
-    id:"Ansh001", 
-    name:"Iphone 12 pro", 
-    desc:"an apple phone", 
-    price:46, 
-    color:"#33505a", 
-    image:"https://images.unsplash.com/photo-1537589376225-5405c60a5bd8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
-  )
-];
